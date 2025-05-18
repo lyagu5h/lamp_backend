f4fa2fe07a47bc4404628f31e0c19d95f7d5d18c
lyagu5h <101698114+lyagu5h@users.noreply.github.com>
2025-05-18 23:00:00 +0300

    app mvp

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -1,12 +1,16 @@
 package domain
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Product struct {
 	ID int `db:"id" json:"id"`
 	Name        string    `db:"name" json:"name"`
     Price       float64   `db:"price" json:"price"`
     Power       int       `db:"power" json:"power"`
+    Description sql.NullString    `db:"description" json:"description"`
     Temperature string    `db:"temperature" json:"temperature"`
     Type        string    `db:"type" json:"type"`
     LampCap     string    `db:"lamp_cap" json:"lamp_cap"`
